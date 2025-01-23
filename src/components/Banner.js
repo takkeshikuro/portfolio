51e32b4debe9f1445dcde7109d047a165dbaa36d
takkeshikuro <takeshi.27@outlook.fr>
2025-01-23 19:55:17 +0100

    wait

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,12 +5,14 @@ function Banner() {
 	let title = "My portfolio";
 	return (<div className='port-banner'>
 		<header>
-			<img src={logo} alt='logo test' className='banner_logo' />
-			<h1>{title}</h1>
-			<nav style={{ textAlign:'right',padding:0.5}}>
-		  		<a href="#about">À propos<br /></a>
-		  		<a href="#projects">Projets<br /></a>
-		  		<a href="#contact">Contact</a>
+			<div className="logo-title">
+				<img src={logo} alt='logo test' className='banner_logo' />
+				<h1>{title}</h1>
+			</div>
+			<nav>
+		  		<a href="#about">About</a>
+		  		<a href="#projects">Projects</a>
+		  		<a href="#contact">Resume</a>
 			</nav>
 	  	</header>
 	</div>);
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,19 +1,34 @@
-const project_list = [
-    'monstera',
-    'ficus lyrata',
-    'pothos argenté',
-    'yucca',
-    'palmier'
-]
+import '../styles/Projects.css';
 
 function Projects() {
-    return (
-        <ul style={{textAlign:'left'}}>
-            {project_list.map((project, index) => (
-                <li key={`${project}-${index}`}>{project}</li>
-            ))}
-        </ul>
-    )
+	const projects_list = [
+		{ title:"Minishell", id: 1, bio:"minishell quoi"},
+		{ title:"FT_IRC", id: 2, bio:"irc en c quoi"},
+		{ title:"CUB3D", id: 3, bio:"jeu quoi"},
+		{ title:"Transcendance", id: 4, bio:"pong quoi"},
+		{ title:"Inception", id: 5, bio:"archi quoi"},
+	];
+	return (
+		<div className="projects-list">
+			<h1>PROJECTS</h1>
+			<div >
+				{projects_list.map((projects_list) => (
+					<ProjectCard key={projects_list.id} 
+						title={projects_list.title}
+						bio={projects_list.bio} />   
+				))}
+			</div>
+		</div>
+	)
 }
 
-export default Projects
+function ProjectCard({title, bio}) {
+	return (
+		<div className="project-card">
+			<h2>{title}</h2>
+			<p>{bio}</p>
+		</div>
+	);
+}
+
+export default Projects;
